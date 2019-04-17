8a60f925e0e5a5a8c451a337ed8756b93dfc4acd
Michael Sass Hansen <michael@brainsquad.ai>
2019-04-18 00:26:22 +0200

    Using pyFileSystem2

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['google-cloud-storage==1.14.0']
+REQUIRED_PACKAGES = ['google-cloud-storage==1.14.0',
+                     'fs==2.4.4',
+                     'fs-gcsfs==0.4.1']
 
 setup(
     name='criterion_core',
