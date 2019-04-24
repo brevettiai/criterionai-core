f7317b6eaf1ee5d9f0c8b4cd99c4e600b980fbdf
Emil Tyge <emtyge@gmail.com>
2019-04-24 14:17:37 +0200

    Empty sample project added

---
+++ sample_model/model/train.py
@@ -0,0 +1,40 @@
+
+import argparse
+import logging
+import os
+
+from criterion_core import CriterionConfig, load_image_datasets
+from criterion_core.utils.sampletools import split_datasets
+
+log = logging.getLogger(__name__)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--job-dir', help='GCS location to write checkpoints and export models', required=True)
+    args, unparsed = parser.parse_known_args()
+    return args, unparsed
+
+
+def train(config):
+    dssamples = load_image_datasets(config.datasets)
+
+    # Split data
+    trainingset, developmentset = split_datasets(dssamples, strategy=config.settings.validation.split_strategy,
+                                                 split=config.settings.validation.split_percentage / 100)
+
+    tmp_package_path = config.temp_path("package.tar.gz")
+
+    log.info("Completing job")
+    config.complete_job(tmp_package_path)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    args, _ = parse_args()
+
+    schema_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings-schema.json')
+    config = CriterionConfig.from_mlengine(args.job_dir, schema_path)
+
+    log.info("Start Training")
+    train(config)
