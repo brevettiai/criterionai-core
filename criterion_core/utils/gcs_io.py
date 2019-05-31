dc56ebdf565f6dc4064e2012811d7f433697ad8d
Michael Sass Hansen <michael@brainsquad.ai>
2019-05-31 16:50:21 +0200

    proper python 3.4+ version of identifying main thread

--- criterion_core/utils/gcs_io.py
+++ criterion_core/utils/gcs_io.py
@@ -29,7 +29,7 @@ def get_signed_url(bucket_id, blob_path, expiration=datetime.datetime.utcnow() +
 
 
 def get_loop():
-    if "Main" in threading.current_thread().name:
+    if threading.current_thread() is threading.main_thread():
         loop = asyncio.get_event_loop()
     else:
         loop = getattr(threadLocal, 'loop', None)
