8b0abd3d8a37905b75cb9ae79a10215f85c1a945
Michael Sass Hansen <michael@brainsquad.ai>
2019-06-15 14:30:28 +0200

    Adding tags to performance tag dictrionary

--- criterion_core/utils/evaluation.py
+++ criterion_core/utils/evaluation.py
@@ -1,5 +1,6 @@
 from criterion_core.utils import path
 import numpy as np
+import copy
 
 
 def get_classification_predictions(model, data_set, data_gen, class_names, output_index=-1):
@@ -29,16 +30,32 @@ def get_classification_summary(class_names, prediction_output, class_weights, cl
     classification_cnt = {}
     for po in prediction_output:
         folder = path.get_folders(po["path"], po["bucket"])[-1]
-        categories = po["category"]
-        accept_class = class_weights in categories
-        accepted = po["prediction"] == class_weights
-        for cat in categories:
-            key = (po["id"], folder, cat)
-            classification_cnt.setdefault(key, dict(accept=0, reject=0))["accept"] += int(accepted) # counting accept
-            classification_cnt[key]["reject"] += int(not accepted)  # counting reject
+        accept_class = class_weights in po["category"]
+        prediction = po["prediction"]
+        accepted = prediction == class_weights
+        key = "-".join((po["id"], folder, prediction))
+        classification_cnt.setdefault(key, {'count': 0, "categories": po["category"], "prediction": prediction,
+                                            "sample_quality": ["reject", "accept"][accept_class], "id": po["id"],
+                                            "Folder": folder,
+                                            "decision": ["reject", "accept"][accepted]})["count"] += 1
     classification_summary = []
     for kk, vv in classification_cnt.items():
-        for pred in ["accept", "reject"]:
-            classification_summary.append(
-                {"id":kk[0], "Folder":kk[1], "class": kk[2], "prediction": pred, 'count': vv[pred]})
+        for category in vv["categories"]:
+            dd = copy.deepcopy(vv)
+            del dd["categories"]
+            dd.update({"class": category, "unique_id": kk})
+            classification_summary.append(dd)
     return classification_summary
+
+def tagged_pivot_classification_summary(classification_summary, ds_tag_records):
+    # adding tags
+    # adding dataset url
+    tagged_classification_summary = []
+    for cs in classification_summary:
+        cs["dataset_url"] = "https://app.criterion.ai/" + cs["id"]
+        tags = ds_tag_records[cs["id"]]
+        for tt in tags:
+            tag_cs = copy.deepcopy(cs)
+            tag_cs.update(tt)
+            tagged_classification_summary.append(tag_cs)
+    return tagged_classification_summary
