24566bb9f21bac196a5b9f4a93d8da79829b7e75
Michael Sass Hansen <michael@brainsquad.ai>
2019-06-25 10:56:08 +0200

    Making vegalite charts for setting security level

--- criterion_core/utils/vegalite_charts.py
+++ criterion_core/utils/vegalite_charts.py
@@ -1,5 +1,44 @@
 import altair as alt
 import pandas as pd
+import numpy as np
+
+
+def make_selector_chart(df, x_name, y_name, selector, color="red", size=100):
+    chart_line = alt.Chart(df).mark_line().encode(
+        x=x_name,
+        y=y_name)
+    chart_sel = alt.Chart(df).mark_circle(color=color, size=size).encode(
+        x=x_name,
+        y=y_name).add_selection(selector).transform_filter(selector)
+    chart_layered = alt.layer(chart_line, chart_sel)
+    return chart_layered
+
+
+def make_security_selection(devel_pred_output, classes):
+    step = 1
+    rng = np.arange(0.0, 100+step, step)
+
+    security_charts = []
+    for cl in classes:
+        sec_level = '{}_security_level'.format(cl)
+        slider = alt.binding_range(min=rng.min(), max=rng.max(), step=step)
+        select_security = alt.selection_single(name="set", fields=[sec_level], bind=slider)
+        scores_accept = np.array([sc["prob_" + cl] for sc in devel_pred_output if cl in sc["category"]])
+        scores_reject = np.array([sc["prob_" + cl] for sc in devel_pred_output if not cl in sc["category"]])
+
+        FRR = [(scores_accept < thr/100).sum()/len(scores_accept ) for thr in rng]
+        TRR = [(scores_reject < thr/100).sum()/len(scores_reject) for thr in rng]
+
+        ROC_df = pd.DataFrame({'FRR': FRR, 'TRR': TRR+1e-5*rng, sec_level: rng})
+
+
+        ROC_comb_alt = make_selector_chart(df=ROC_df, x_name='TRR', y_name='FRR', selector=select_security)
+        FRR_comb_alt = make_selector_chart(df=ROC_df, x_name=sec_level, y_name='FRR', selector=select_security)
+        TRR_comb_alt = make_selector_chart(df=ROC_df, x_name=sec_level, y_name='TRR', selector=select_security)
+
+        security_charts.append(alt.concat(ROC_comb_alt, FRR_comb_alt, TRR_comb_alt).resolve_scale(color='independent').to_json())
+    return security_charts
+
 
 def dataset_summary(samples):
     data = pd.DataFrame(list(samples))
